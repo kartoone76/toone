32a98e7318166e4d08e9a5b01145330142a5a8d3
Brian Toone <115804135+kartoone76@users.noreply.github.com>
2022-11-04 14:24:26 +0000

    worked on localstorage

--- script.js
+++ script.js
@@ -8,9 +8,46 @@ function submitForm(name, hometown) {
   var guestlist = document.getElementById("guestlist");
   var n = document.getElementById("name");
   var h = document.getElementById("hometown");
+  var currentguestlist = localStorage.getItem("guestlist");
+  localStorage.setItem("guestlist", currentguestlist + "<li>" + name + ". " + hometown + "</li>");
   guestlist.innerHTML += "<li>" + name + ". " + hometown + "</li>";
   n.value = "";
   h.value = "";
   n.focus();
+  return false;
+}
+
+function clearStorageGB() {
+  var guestlist = document.getElementById("guestlist");
+  guestlist.innerHTML = "";
+  localStorage.setItem("guestlist","");
+}
+
+function checkStorageGB() {
+  var guestlist = document.getElementById("guestlist");
+  var currentguestlist = localStorage.getItem("guestlist");
+  guestlist.innerHTML = currentguestlist;
+}
+
+function clearStorage() {
+  var answerlist = document.getElementById("answerlist");
+  answerlist.innerHTML = "";
+  localStorage.setItem("answerlist","");
+}
+
+function checkStorage() {
+  var answerlist = document.getElementById("answerlist");
+  var currentanswers = localStorage.getItem("answerlist");
+  answerlist.innerHTML = currentanswers;
+}
+
+function submitAnswer(answer) {
+  // search the DOM for a specific tag with the id "answerlist"
+  var answerlist = document.getElementById("answerlist");
+  var currentanswers = localStorage.getItem("answerlist");
+  localStorage.setItem("answerlist", currentanswers + "<br />" + answer.value);
+  answerlist.innerHTML += "<br />" + answer.value;
+  answer.value = "";
+  answer.focus();
   return false;
 }
